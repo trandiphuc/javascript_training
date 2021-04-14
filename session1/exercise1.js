c7bbd234be82471c7f54db45711ff00447ea6471
trandiphuc <62286462+trandiphuc@users.noreply.github.com>
2021-04-14 13:50:02 +0700

    Update exercise1.js

--- session1/exercise1.js
+++ session1/exercise1.js
@@ -1,15 +1,28 @@
-function numberWithCommas(number) {
-    var regex = /\B(?=(\d{3})+(?!\d))/g;
-    var result = number.toString();
-    return result.replace(regex, ",");
+//Ex1
+function reverseString(str) {
+    return str.split("").reverse().join("");
 }
-
+function numberWithCommas(number){
+        var result = "";
+        var str = number.toString();
+        var count = 0;
+        for(var i = str.length - 1; i >= 0; i--){
+            result += str[i];
+            count++;
+            if(count === 3)
+            {
+                result += ',';
+                count =0;
+            }
+        }
+        return reverseString(result);
+}
+//Ex2
 function shortenMoney(number) {
     var tempNum = number;
     var count = 0;
     var res = 0;
-    while(tempNum / 1000 >= 1)
-    {
+    while(tempNum / 1000 >= 1) {
         count++; 
         tempNum /= 1000;
     }
@@ -29,6 +42,7 @@ function shortenMoney(number) {
             return res;
     }
 }
+//Ex3
 function isUpperCase(char) {
     return char.toUpperCase() === char;
 }
@@ -43,27 +57,8 @@ function countWords(string) {
     }
     return count;
 }
-
-function reverseString(str) {
-    return str.split("").reverse().join("");
-}
-function formatNumber(number){
-        var result = "";
-        var str = number.toString();
-        var count = 0;
-        for(var i = str.length - 1; i >= 0; i--){
-            result += str[i];
-            count++;
-            if(count === 3)
-            {
-                result += ',';
-                count =0;
-            }
-        }
-        return reverseString(result);
-}
-
-function getExtensionName(fileName){
+//Ex4
+function getExtensionName(fileName) {
     var posExtensionName = fileName.lastIndexOf('.');
     extensionName = fileName.slice(posExtensionName + 1, fileName.length);
     return extensionName;
