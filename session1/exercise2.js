358e320165f60a7c1bec412f05ba234f5f622c19
trandiphuc <62286462+trandiphuc@users.noreply.github.com>
2021-04-14 13:52:45 +0700

    Update exercise2.js

--- session1/exercise2.js
+++ session1/exercise2.js
@@ -1,16 +1,20 @@
+//Ex1
 function calFactorialNumber(number)
 {
     if(number >= 1)
         return (number * calFactorialNumber(number - 1));
     else return 1;
 }
+//Ex2
 function getRandomInt(min, max){
     return Math.floor((Math.random() * (max - min + 1)) + min);
 }
+//Ex3
 function getRandomElementInArray(array)
 {
     return array[Math.floor(Math.random() * array.length)];
 }
+//Ex4
 function findMissingElement(arr1, arr2)
 {
     var result = [];
