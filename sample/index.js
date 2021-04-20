134c7d01694741ae8a41eeee198622adb7927a37
Gemi <gemi@Gemis-MacBook-Pro.local>
2021-04-20 18:02:08 +0700

    update

--- sample/index.js
+++ sample/index.js
@@ -1,13 +1,7 @@
 import {Game} from "./modules/Game.js";
 
 var game = new Game();
-var btn = document.createElement("button");
-btn.textContent = "Play Again";
-btn.style.position = "absolute";
-btn.style.top = "100px";
-btn.onclick = function() {
-    location.reload();
-    game.init()};
+
 document.body.appendChild(game.elm);
-document.body.appendChild(btn);
+
 game.init();
